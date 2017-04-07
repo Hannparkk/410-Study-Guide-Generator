3a0aa2d10cc302d52b4b3204d68eb4b5262224d8
Hanna Parker <hparker2@illinois.edu>
2017-04-07 15:20:46 -0500

    crawls wiki a lil

--- crawler.py
+++ crawler.py
@@ -2,21 +2,12 @@ import requests
 from BeautifulSoup import BeautifulSoup
 
 #get search results
-query_word = "whales"
-url = "https://quizlet.com/subject/" + query_word + "/"
+query_word = "Whale"
+url = "https://en.wikipedia.org/wiki/" + query_word
 response = requests.get(url)
 html = response.content
 soup = BeautifulSoup(html)
-search_results_list = soup.findAll("a", { "class" : "UILink" })
 
-#scrape those search results
-for link in search_results_list:
-    url = link['href']
-    response = requests.get(url)
-    html = response.content
-    soup = BeautifulSoup(html)
-    list_of_words = soup.findAll('span', {"class" : "TermText notranslate lang-en qWord"})
-    list_of_defs = soup.findAll('span', {"class" : "TermText notranslate lang-en qDef"})
-    flashcards = {}
-    for set in list_of_words:
-        flashcards[list_of_words[set]] = list_of_defs[set]
+scrape_anything = soup.findAll("h2")
+for header in scrape_anything:
+    print header.text
