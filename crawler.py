c2a0140aa676c33b5d6976dd50202e4bc427e45d
Hanna Parker <hparker2@illinois.edu>
2017-04-07 15:43:30 -0500

    Method returns full doc text

--- crawler.py
+++ crawler.py
@@ -2,11 +2,11 @@ import requests
 from BeautifulSoup import BeautifulSoup
 
 #get search results
-query_word = "Whale"
-url = "https://en.wikipedia.org/wiki/" + query_word
-response = requests.get(url)
-html = response.content
-soup = BeautifulSoup(html)
+def search(url):
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html)
 
-body = soup.findAll("body")
-full_text = body[0].text
+    body = soup.findAll("body")
+    full_text = body[0].text
+    return full_text
