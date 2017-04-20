8f607059ae1724490ccef1efba35e0cc2615176d
dnagarkar <devashri@gmail.com>
2017-04-19 22:05:13 -0500

    trg

--- dictionary.py
+++ dictionary.py
@@ -22,119 +22,122 @@
 
 import re
 
-d = [dict() for x in range(17)]
+def dictionary():
 
-text = ''.join(open("whale_data/whale_doc1", "r").readlines())
-sentences = re.split(r' *[\.\?!][\'"\)\]]* +', text)
+	d = [dict() for x in range(17)]
 
-d[0] = {sentences[i]: "https://en.wikipedia.org/wiki/Whale" for i in range(0, len(sentences))}
+	text = ''.join(open("whale_data/whale_doc1", "r").readlines())
+	sentences = re.split(r' *[\.\?!][\'"\)\]]* +', text)
 
-# text.close()
+	d[0] = {sentences[i]: "https://en.wikipedia.org/wiki/Whale" for i in range(0, len(sentences))}
 
-text1 = ''.join(open("whale_data/whale_doc2", "r").readlines())
-sentences1 = re.split(r' *[\.\?!][\'"\)\]]* +', text1)
+	# text.close()
 
-d[1] = {sentences1[i]: "https://en.wikipedia.org/wiki/List_of_cetacean_species" for i in range(0, len(sentences1))}
+	text1 = ''.join(open("whale_data/whale_doc2", "r").readlines())
+	sentences1 = re.split(r' *[\.\?!][\'"\)\]]* +', text1)
 
-# text1.close()
+	d[1] = {sentences1[i]: "https://en.wikipedia.org/wiki/List_of_cetacean_species" for i in range(0, len(sentences1))}
 
-text2 = ''.join(open("whale_data/whale_doc3", "r").readlines())
-sentences2 = re.split(r' *[\.\?!][\'"\)\]]* +', text2)
+	# text1.close()
 
-d[2] = {sentences2[i]: "https://en.wikipedia.org/wiki/Cetacea" for i in range(0, len(sentences2))}
+	text2 = ''.join(open("whale_data/whale_doc3", "r").readlines())
+	sentences2 = re.split(r' *[\.\?!][\'"\)\]]* +', text2)
 
-# text2.close()
+	d[2] = {sentences2[i]: "https://en.wikipedia.org/wiki/Cetacea" for i in range(0, len(sentences2))}
 
-text3 = ''.join(open("whale_data/whale_doc4", "r").readlines())
-sentences3 = re.split(r' *[\.\?!][\'"\)\]]* +', text3)
+	# text2.close()
 
-d[3] = {sentences3[i]: "https://en.wikipedia.org/wiki/Killer_whale" for i in range(0, len(sentences3))}
+	text3 = ''.join(open("whale_data/whale_doc4", "r").readlines())
+	sentences3 = re.split(r' *[\.\?!][\'"\)\]]* +', text3)
 
-# text3.close()
+	d[3] = {sentences3[i]: "https://en.wikipedia.org/wiki/Killer_whale" for i in range(0, len(sentences3))}
 
-text4 = ''.join(open("whale_data/whale_doc5", "r").readlines())
-sentences4 = re.split(r' *[\.\?!][\'"\)\]]* +', text4)
+	# text3.close()
 
-d[4] = {sentences4[i]: "https://en.wikipedia.org/wiki/Humpback_whale" for i in range(0, len(sentences4))}
+	text4 = ''.join(open("whale_data/whale_doc5", "r").readlines())
+	sentences4 = re.split(r' *[\.\?!][\'"\)\]]* +', text4)
 
-# text4.close()
+	d[4] = {sentences4[i]: "https://en.wikipedia.org/wiki/Humpback_whale" for i in range(0, len(sentences4))}
 
-text5 = ''.join(open("whale_data/whale_doc6", "r").readlines())
-sentences5 = re.split(r' *[\.\?!][\'"\)\]]* +', text5)
+	# text4.close()
 
-d[5] = {sentences5[i]: "https://en.wikipedia.org/wiki/Baleen_whale" for i in range(0, len(sentences5))}
+	text5 = ''.join(open("whale_data/whale_doc6", "r").readlines())
+	sentences5 = re.split(r' *[\.\?!][\'"\)\]]* +', text5)
 
-# text5.close()
+	d[5] = {sentences5[i]: "https://en.wikipedia.org/wiki/Baleen_whale" for i in range(0, len(sentences5))}
 
-text6 = ''.join(open("whale_data/whale_doc7", "r").readlines())
-sentences6 = re.split(r' *[\.\?!][\'"\)\]]* +', text6)
+	# text5.close()
 
-d[6] = {sentences6[i]: "https://en.wikipedia.org/wiki/Pilot_whale" for i in range(0, len(sentences6))}
+	text6 = ''.join(open("whale_data/whale_doc7", "r").readlines())
+	sentences6 = re.split(r' *[\.\?!][\'"\)\]]* +', text6)
 
-# text6.close()
+	d[6] = {sentences6[i]: "https://en.wikipedia.org/wiki/Pilot_whale" for i in range(0, len(sentences6))}
 
-text7 = ''.join(open("whale_data/whale_doc8", "r").readlines())
-sentences7 = re.split(r' *[\.\?!][\'"\)\]]* +', text7)
+	# text6.close()
 
-d[7] = {sentences7[i]: "https://en.wikipedia.org/wiki/Sperm_whale" for i in range(0, len(sentences7))}
+	text7 = ''.join(open("whale_data/whale_doc8", "r").readlines())
+	sentences7 = re.split(r' *[\.\?!][\'"\)\]]* +', text7)
 
-# text7.close()
+	d[7] = {sentences7[i]: "https://en.wikipedia.org/wiki/Sperm_whale" for i in range(0, len(sentences7))}
 
-text8 = ''.join(open("whale_data/whale_doc9", "r").readlines())
-sentences8 = re.split(r' *[\.\?!][\'"\)\]]* +', text8)
+	# text7.close()
 
-d[8] = {sentences8[i]: "https://en.wikipedia.org/wiki/Sei_whale" for i in range(0, len(sentences8))}
+	text8 = ''.join(open("whale_data/whale_doc9", "r").readlines())
+	sentences8 = re.split(r' *[\.\?!][\'"\)\]]* +', text8)
 
-# text8.close()
+	d[8] = {sentences8[i]: "https://en.wikipedia.org/wiki/Sei_whale" for i in range(0, len(sentences8))}
 
-text9 = ''.join(open("whale_data/whale_doc10", "r").readlines())
-sentences9 = re.split(r' *[\.\?!][\'"\)\]]* +', text9)
+	# text8.close()
 
-d[9] = {sentences9[i]: "https://en.wikipedia.org/wiki/Blue_whale" for i in range(0, len(sentences9))}
+	text9 = ''.join(open("whale_data/whale_doc10", "r").readlines())
+	sentences9 = re.split(r' *[\.\?!][\'"\)\]]* +', text9)
 
-# text.close()
+	d[9] = {sentences9[i]: "https://en.wikipedia.org/wiki/Blue_whale" for i in range(0, len(sentences9))}
 
-text10 = ''.join(open("whale_data/whale_doc11", "r").readlines())
-sentences10 = re.split(r' *[\.\?!][\'"\)\]]* +', text10)
+	# text.close()
 
-d[10] = {sentences10[i]: "https://en.wikipedia.org/wiki/Captive_killer_whales" for i in range(0, len(sentences10))}
+	text10 = ''.join(open("whale_data/whale_doc11", "r").readlines())
+	sentences10 = re.split(r' *[\.\?!][\'"\)\]]* +', text10)
 
-
-text11 = ''.join(open("whale_data/whale_doc12", "r").readlines())
-sentences11 = re.split(r' *[\.\?!][\'"\)\]]* +', text11)
-
-d[11] = {sentences11[i]: "https://en.wikipedia.org/wiki/Walnut_Whales" for i in range(0, len(sentences11))}
+	d[10] = {sentences10[i]: "https://en.wikipedia.org/wiki/Captive_killer_whales" for i in range(0, len(sentences10))}
 
 
-text13 = ''.join(open("whale_data/whale_doc13", "r").readlines())
-sentences13 = re.split(r' *[\.\?!][\'"\)\]]* +', text13)
+	text11 = ''.join(open("whale_data/whale_doc12", "r").readlines())
+	sentences11 = re.split(r' *[\.\?!][\'"\)\]]* +', text11)
 
-d[12] = {sentences13[i]: "https://en.wikipedia.org/wiki/Evolution_of_cetaceans" for i in range(0, len(sentences13))}
-
-text14 = ''.join(open("whale_data/whale_doc14", "r").readlines())
-sentences14 = re.split(r' *[\.\?!][\'"\)\]]* +', text14)
-
-d[13] = {sentences14[i]: "https://en.wikipedia.org/wiki/Marine_mammal" for i in range(0, len(sentences14))}
-
-text15 = ''.join(open("whale_data/whale_doc15", "r").readlines())
-sentences15 = re.split(r' *[\.\?!][\'"\)\]]* +', text15)
-
-d[14] = {sentences15[i]: "https://en.wikipedia.org/wiki/Aquatic_mammal" for i in range(0, len(sentences15))}
-
-text16 = ''.join(open("whale_data/whale_doc16", "r").readlines())
-sentences16 = re.split(r' *[\.\?!][\'"\)\]]* +', text16)
-
-d[15] = {sentences16[i]: "https://en.wikipedia.org/wiki/Whale_shark" for i in range(0, len(sentences16))}
-
-text17 = ''.join(open("whale_data/whale_doc17", "r").readlines())
-sentences17 = re.split(r' *[\.\?!][\'"\)\]]* +', text17)
-
-d[16] = {sentences17[i]: "https://en.wikipedia.org/wiki/Beluga_whale" for i in range(0, len(sentences17))}
+	d[11] = {sentences11[i]: "https://en.wikipedia.org/wiki/Walnut_Whales" for i in range(0, len(sentences11))}
 
 
-super_dict = {}
-for a in d:
-    for k, v in a.iteritems():  # d.items() in Python 3+
-        super_dict.setdefault(k, []).append(v)
+	text13 = ''.join(open("whale_data/whale_doc13", "r").readlines())
+	sentences13 = re.split(r' *[\.\?!][\'"\)\]]* +', text13)
 
-print(super_dict)
+	d[12] = {sentences13[i]: "https://en.wikipedia.org/wiki/Evolution_of_cetaceans" for i in range(0, len(sentences13))}
+
+	text14 = ''.join(open("whale_data/whale_doc14", "r").readlines())
+	sentences14 = re.split(r' *[\.\?!][\'"\)\]]* +', text14)
+
+	d[13] = {sentences14[i]: "https://en.wikipedia.org/wiki/Marine_mammal" for i in range(0, len(sentences14))}
+
+	text15 = ''.join(open("whale_data/whale_doc15", "r").readlines())
+	sentences15 = re.split(r' *[\.\?!][\'"\)\]]* +', text15)
+
+	d[14] = {sentences15[i]: "https://en.wikipedia.org/wiki/Aquatic_mammal" for i in range(0, len(sentences15))}
+
+	text16 = ''.join(open("whale_data/whale_doc16", "r").readlines())
+	sentences16 = re.split(r' *[\.\?!][\'"\)\]]* +', text16)
+
+	d[15] = {sentences16[i]: "https://en.wikipedia.org/wiki/Whale_shark" for i in range(0, len(sentences16))}
+
+	text17 = ''.join(open("whale_data/whale_doc17", "r").readlines())
+	sentences17 = re.split(r' *[\.\?!][\'"\)\]]* +', text17)
+
+	d[16] = {sentences17[i]: "https://en.wikipedia.org/wiki/Beluga_whale" for i in range(0, len(sentences17))}
+
+
+	super_dict = {}
+	for a in d:
+	    for k, v in a.iteritems():  # d.items() in Python 3+
+	        super_dict.setdefault(k, []).append(v)
+
+	return super_dict
+
