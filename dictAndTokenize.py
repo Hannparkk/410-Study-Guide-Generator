4228e67a0124d3d0d331881cc8ac3de590247051
Salina Ortega <sortega2@illinois.edu>
2017-04-28 22:25:53 -0500

    changes

--- dictAndTokenize.py
+++ dictAndTokenize.py
@@ -1,4 +1,4 @@
-from dictionary import dictionary1
+from dictionary import dictionary
 import metapy
 from subprocess import call
 import sys
@@ -9,17 +9,21 @@ import metapy
 import numpy
 import shutil
 from lda_reader import LDAReader
+from removeStopWords import remStopWords
 #import tokenizeDict
 
-super_dict = dictionary1()
+super_dict = dictionary()
+super_dict = remStopWords(super_dict)
 
 shutil.rmtree('nips00/fwd') #deletes folder
 f = open("nips00/nips00.dat", "w+") 
 for key in super_dict:
 	f.write(key + "\n") #write each line of the dictionary to the file to be read by the lda
+#remove stop words?????
+
 #run lda
 o = open("out.txt", "w+")
-lda = metapy.topics.run_gibbs("config.toml", "lda_model", 5, alpha=0.1, beta=0.1, num_iters=500)
+lda = metapy.topics.run_gibbs("config.toml", "lda_model", 7, alpha=0.1, beta=0.1, num_iters=500)
 reader = LDAReader("config.toml", "lda_model") #interpret the lda output
 for topic_id in range(reader.num_topics()):
 	print("{} Topic {} {}".format('=' * 10, topic_id, '=' * 10))
@@ -29,10 +33,3 @@ for topic_id in range(reader.num_topics()):
 		print(" {} {}".format(term, prob))
 		o.write(" {} {}".format(term, prob))
 		o.write("\n")
-for i in range(5):
-	print("{} doc_id {} topic distribution {}".format('=' * 10, i, '=' * 10))
-   	print(reader.topic_dist(i))
-   	o.write("{} doc_id {} topic distribution {}".format('=' * 10, i, '=' * 10))
-   	o.write("\n")
-   	o.write(reader.topic_dist(i))
-   	o.write("\n")
