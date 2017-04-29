cea1e8f5364ed90c0908cf5fea1403522f2f1b2b
Salina Ortega <sortega2@illinois.edu>
2017-04-28 23:14:25 -0500

    ran lda

--- m.py
+++ m.py
@@ -1,8 +1,10 @@
 from dictionary import *
 from find_bigrams import *
 from lda_parser import *
+from dictAndTokenize import *
 # from find_bigrams import
 
+runLDA()
 a= dictionary()
 array= parse("out.txt")
 find_bigram_array(array, a)
