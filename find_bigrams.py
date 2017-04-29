6ec50407660fc85ddbb8eda8725cdb289dbc25e6
dnagarkar <devashri@gmail.com>
2017-04-28 23:29:53 -0500

    added arrays and shit

--- find_bigrams.py
+++ find_bigrams.py
@@ -1,14 +1,24 @@
-def find_bigram(bigram, superdict):
+def find_bigram(bigram, superdict, a1, a2):
 	#bigram= bigram.replace("_", " ");
 	#single character words
 	bigram = " " + bigram + " "
 	print bigram #get rid of this line eventually, but its fine for now
 	for k, v in superdict.iteritems():  # d.items() in Python 3
 		if bigram in k:
-			print(k, v)
-			break
+			a1.append(k)
+			a2.append(v)
 
 
 def find_bigram_array(array, superdict):
 	for b in array:
-		find_bigram(b, superdict)
+		a1=[]
+		a2=[]
+		find_bigram(b, superdict, a1, a2)
+		print a1[10]
+		print a2[10]
+
+
+	# a=[]
+	# for b in array:
+	# 	find_bigram(b, superdict, a)
+	# print a[10]
